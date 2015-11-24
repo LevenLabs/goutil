bff205bd929a5d8b9d65f1d986d109dec09a9ade
Brian Picciano <mediocregopher@gmail.com>
2015-11-24 16:18:42 -0700

    implement rpc-cli

---
+++ rpc-cli/README.md
@@ -0,0 +1,5 @@
+# rpc-cli
+
+A simple binary for performing JSON RPC 2 calls against arbitrary api endpoints
+
+> rpc-cli 'http://localhost:5555/api' 'Foo.DoAThing' '{"foo":"bar"}'
---
+++ rpc-cli/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/levenlabs/golib/rpcutil"
+)
+
+func main() {
+	bin := os.Args[0]
+	argv := os.Args[1:]
+	if len(argv) != 3 {
+		fmt.Printf("Usage: %s <url> <service.method> <parameters>\n\n", bin)
+		fmt.Printf(`Example: %s 'http://localhost:5555/api' 'Foo.DoAThing' '{"foo":"bar"}'`, bin)
+		fmt.Print("\n\n")
+	}
+
+	u, method, body := argv[0], argv[1], argv[2]
+	var ret interface{}
+	err := rpcutil.JSONRPC2RawCall(u, &ret, method, body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	out, err := json.MarshalIndent(ret, "", "    ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(out))
+}
