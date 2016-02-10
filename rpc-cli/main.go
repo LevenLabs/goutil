000f2f9d86be706ef1fd1b6a218ec97fd97d6815
Brian Picciano <mediocregopher@gmail.com>
2016-02-10 14:21:30 -0700

    make rpc-cli use lever and llog, make it possible to output result as an INFO message

--- rpc-cli/main.go
+++ rpc-cli/main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/url"
 	"os"
+	"time"
 
+	"github.com/levenlabs/go-llog"
 	"github.com/levenlabs/go-srvclient"
 	"github.com/levenlabs/golib/rpcutil"
-	"net/url"
+	"github.com/mediocregopher/lever"
 )
 
 func main() {
-	bin := os.Args[0]
-	argv := os.Args[1:]
+	l := lever.New("rpc-cli", &lever.Opts{
+		HelpHeader:         "Usage: rpc-cli [options] <url> <service.method> <parameters>\n",
+		DisallowConfigFile: true,
+	})
+	l.Add(lever.Param{
+		Name:        "--llog",
+		Description: "Instead of outputting the return as json, output a llog-style message with the returned json object as the key/value params",
+		Flag:        true,
+	})
+	l.Parse()
+
+	argv := l.ParamRest()
 	if len(argv) != 3 {
-		fmt.Printf("Usage: %s <url> <service.method> <parameters>\n\n", bin)
-		fmt.Printf(`Example: %s 'http://localhost:5555/api' 'Foo.DoAThing' '{"foo":"bar"}'`, bin)
-		fmt.Print("\n\n")
-		return
+		fmt.Print(l.Help())
+		exit(0)
 	}
 
 	u, method, body := argv[0], argv[1], argv[2]
@@ -28,15 +39,30 @@ func main() {
 	var ret interface{}
 	err := rpcutil.JSONRPC2RawCall(u, &ret, method, body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		llog.Error("error calling rpc method", llog.KV{"err": err})
+		exit(1)
+	}
+
+	if l.ParamFlag("--llog") {
+		retm, ok := ret.(map[string]interface{})
+		if !ok {
+			llog.Error("return value not a json object, can't llog")
+			exit(1)
+		}
+		llog.Info("output from "+method, llog.KV(retm))
+		exit(0)
 	}
 
 	out, err := json.MarshalIndent(ret, "", "    ")
 	if err != nil {
-		fmt.Println(err)
-		return
+		llog.Error("error marshalling json", llog.KV{"err": err})
+		exit(1)
 	}
 
 	fmt.Println(string(out))
+}
+
+func exit(i int) {
+	time.Sleep(100 * time.Millisecond)
+	os.Exit(i)
 }
