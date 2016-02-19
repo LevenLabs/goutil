1d95cb0d105246ff3d847bd88f1e4c76fd421d58
Brian Picciano <mediocregopher@gmail.com>
2016-02-19 13:24:28 -0700

    automatically prepend http:// to rpc-cli urls if not given

--- rpc-cli/main.go
+++ rpc-cli/main.go
@@ -34,8 +34,12 @@ func main() {
 	u, method, body := argv[0], argv[1], argv[2]
 	if uo, err := url.Parse(u); err == nil {
 		uo.Host = srvclient.MaybeSRV(uo.Host)
+		if uo.Scheme == "" {
+			uo.Scheme = "http"
+		}
 		u = uo.String()
 	}
+
 	var ret interface{}
 	err := rpcutil.JSONRPC2RawCall(u, &ret, method, body)
 	if err != nil {
